a8529de686db5877584a983645e8014492c1fd25
Andres Latorre <91092724+Colomboo1@users.noreply.github.com>
2022-04-10 17:29:13 -0300

    adicion de la correcion de mati

--- array.js
+++ array.js
@@ -1,6 +1,7 @@
 
 const productos=[
     Mesa335={
+        id: 1,
         nombre:"pan de Mesa Lactal 335g",
         molde:5,
         peso:335,
@@ -13,6 +14,7 @@ const productos=[
 
     },
     Salvado350={
+        id: 2,
         nombre:"salvado chico Lactal 350g",
         molde:5,
         peso:410,
@@ -25,6 +27,7 @@ const productos=[
 
     },	
     SemillasLactal={
+        id: 3,
         nombre:"pan semillas Lactal 370g",
         molde:5,
         peso:410,
@@ -37,6 +40,7 @@ const productos=[
 
     },	
     LacteoChico={
+        id: 4,
         nombre:"pan lacteo chico Fargo 470g ",
         molde:5,
         peso:515,
@@ -49,6 +53,7 @@ const productos=[
 
     },	
     RusticoBriosh={
+        id: 5,
         nombre:"Pan Rustico Briosh 500g",
         molde:5,
         peso:570,
@@ -61,6 +66,7 @@ const productos=[
 
     },	
     Liviano={
+        id: 6,
         nombre:"pan liviano Bimbo 400g",
         molde:5,
         peso:465,
@@ -73,6 +79,7 @@ const productos=[
 
     },	
     PanBlanco={
+        id: 7,
         nombre:"pan blanco bimbo 320g",
         molde:4,
         peso:360,
@@ -84,6 +91,7 @@ const productos=[
         marca:"fargo"
     }, 
     SalvadoF={ 
+        id: 8,
         nombre:"pan de salvadp Fargo 350g",
         molde:4,
         peso:415,
@@ -95,6 +103,7 @@ const productos=[
         marca:"fargo"
     }, 	
     DobleIntegral={
+        id: 9,
         nombre:"pan doble integral fargo 440g",
         molde:4,
         peso:510,
@@ -106,6 +115,7 @@ const productos=[
         marca:"fargo"
     },
     MixCereal={
+        id: 10,
         nombre:"pan mixcereal fargo 430g",
         molde:4,
         peso:495,
@@ -117,6 +127,7 @@ const productos=[
         marca:"fargo"
     },
     Lino={
+        id: 11,
         nombre:"pan de lino bimbo 350g",
         molde:4,
         peso:435,
@@ -128,6 +139,7 @@ const productos=[
         marca:"bimbo"
     },
     Integral={
+        id: 12,
         nombre:"pan integral bimbo 320g",
         molde:4,
         peso:415,
@@ -139,6 +151,7 @@ const productos=[
         marca:"bimbo"
     },
     ArtesanoSemilas={
+        id: 13,
         nombre:"Artesano semillas bimbo 400g",
         molde:4,
         peso:460,
@@ -150,6 +163,7 @@ const productos=[
         marca:"bimbo"
     },
     Artesano={
+        id: 14,
         nombre:"Artesano bimbo 400g",
         molde:4,
         peso:450,
@@ -161,3 +175,5 @@ const productos=[
         marca:"bimbo"
     }
 ]
+
+module.exports = { productosA };
--- index.js
+++ index.js
@@ -1,10 +1,45 @@
 
-function loadoven() {
+// SE POPULA EL SELECT ANTES DE QUE CARGUE LA PAGINA
+window.onload = () => {
+    populateProducts();
+};
+
+
+// SE POPULA EL SELECT DINAMICAMENTE CON PRODUCTOS PROVENIENTES DEL ARRAY EN ARRAY.JS
+const populateProducts = () => {
+    const products = document.getElementById("productList");
+    productos.map(producto => {
+        const option = document.createElement("option");
+        option.value = producto.id;
+        option.innerText = producto.nombre;
+        products.appendChild(option);
+    });
+};
+
+// SE LLAMA AL PRODUCTO SELECCIONADO DEL ARRAY Y SE GUARDA 
+function getElementOfList() {
     
-    let tmold=Number(prompt("el molde tiene 4 cavidades o 5?"));
-    let blow=Number(prompt("a cuantos golpes va la divisora?"));
-    let time=Number(prompt("cuanto es el tiempo de horneo"));
+    const valueList = document.getElementById("productList").value;
+    const producto= productos.filter((producto) => producto.id==valueList);
+    console.log("llamada funcion");
+    console.log(producto);
+
+    return producto;
+    
+}
+// destructuracion del objeto
+const destructuracion = () => { 
+    console.log(id);
+    console.log(nombre);
+    const {id, nombre, molde, peso, conTapa, tiempoHorneo, tiempoEnf, golpes, rebanadas, marca} = getElementOfList() 
+}
+
+function loadoven(blow,tmold,time) {
+
+    console.log("llamada funcion ladoven ", producto.molde);
+    console.log("tmold ", tmold)
     let loader= parseInt(((blow/tmold)*time)/32);
+    console.log
     console.log(tmold,blow,time)
     return (
         alert(loader+" Son los moldes que debe cargar")
@@ -38,183 +73,23 @@ console.log(nombres);
 //alert(` Los productos para elegir son: \n ${nombres} `)
 }
 
-// Recibo por parámetro el array
+// ------------------------>>>>>>>>>>>>> COMENTADO POR MATI
+/* // Recibo por parámetro el array
 function productListByHTML(productos) {
     // contenedor asignado
     let contenedor = document.getElementById("mold");
-  
+
     // Recorremos el array y por cada item para el option
     for (const producto of productos) {
-      // contenedor individual
-      let option = document.createElement("option");
-  
-      option.innerHTML = `<option value=${produto.mold}>${prodcuto.name}</option>`;
-  
-      //agregamos al contenedor  desde el HTML
-      contenedor.appendChild(option);
+        // contenedor individual
+        let option = document.createElement("option");
+
+        option.innerHTML = `<option value=${produto.mold}>${prodcuto.name}</option>`;
+
+        //agregamos al contenedor  desde el HTML
+        contenedor.appendChild(option);
     }
-  }
+} */
 
 
-const productos=[
-    Mesa335={
-        nombre:"Pan de Mesa Lactal 335g",
-        molde:5,
-        peso:335,
-        conTapa:false,
-        tiempoHorneo:20,
-        tiempoEnf:60,
-        golpes:112,
-        rebanadas:10,
-        marca:"Lactal"
 
-    },
-    Salvado350={
-        nombre:"Salvado chico Lactal 350g",
-        molde:5,
-        peso:410,
-        conTapa:false,
-        tiempoHorneo:20,
-        tiempoEnf:60,
-        golpes:100,
-        rebanadas:10,
-        marca:"Lactal"
-
-    },	
-    SemillasLactal={
-        nombre:"Pan semillas Lactal 370g",
-        molde:5,
-        peso:410,
-        conTapa:false,
-        tiempoHorneo:20,
-        tiempoEnf:60,
-        golpes:100,
-        rebanadas:10,
-        marca:"Lactal"
-
-    },	
-    LacteoChico={
-        nombre:"Pan lacteo chico Fargo 470g ",
-        molde:5,
-        peso:515,
-        conTapa:true,
-        tiempoHorneo:20,
-        tiempoEnf:60,
-        golpes:90,
-        rebanadas:10,
-        marca:"fargo"
-
-    },	
-    RusticoBriosh={
-        nombre:"Pan Rustico Briosh 500g",
-        molde:5,
-        peso:570,
-        conTapa:false,
-        tiempoHorneo:20,
-        tiempoEnf:60,
-        golpes:82,
-        rebanadas:10,
-        marca:"briosh"
-
-    },	
-    Liviano={
-        nombre:"Pan liviano Bimbo 400g",
-        molde:5,
-        peso:465,
-        conTapa:true,
-        tiempoHorneo:20,
-        tiempoEnf:60,
-        golpes:98,
-        rebanadas:10,
-        marca:"bimbo"
-
-    },	
-    PanBlanco={
-        nombre:"Pan blanco bimbo 320g",
-        molde:4,
-        peso:360,
-        conTapa:false,
-        tiempoHorneo:19,
-        tiempoEnf:60,
-        golpes:93,
-        rebanadas:10,
-        marca:"fargo"
-    }, 
-    SalvadoF={ 
-        nombre:"Pan de salvadp Fargo 350g",
-        molde:4,
-        peso:415,
-        conTapa:false,
-        tiempoHorneo:19,
-        tiempoEnf:60,
-        golpes:93,
-        rebanadas:10,
-        marca:"fargo"
-    }, 	
-    DobleIntegral={
-        nombre:"Pan doble integral fargo 440g",
-        molde:4,
-        peso:510,
-        conTapa:false,
-        tiempoHorneo:21,
-        tiempoEnf:60,
-        golpes:84,
-        rebanadas:10,
-        marca:"fargo"
-    },
-    MixCereal={
-        nombre:"Pan mixcereal fargo 430g",
-        molde:4,
-        peso:495,
-        conTapa:false,
-        tiempoHorneo:21,
-        tiempoEnf:60,
-        golpes:84,
-        rebanadas:10,
-        marca:"fargo"
-    },
-    Lino={
-        nombre:"Pan de lino bimbo 350g",
-        molde:4,
-        peso:435,
-        conTapa:false,
-        tiempoHorneo:19,
-        tiempoEnf:60,
-        golpes:93,
-        rebanadas:10,
-        marca:"bimbo"
-    },
-    Integral={
-        nombre:"Pan integral bimbo 320g",
-        molde:4,
-        peso:415,
-        conTapa:false,
-        tiempoHorneo:19,
-        tiempoEnf:60,
-        golpes:93,
-        rebanadas:10,
-        marca:"bimbo"
-    },
-    ArtesanoSemilas={
-        nombre:"Artesano semillas bimbo 400g",
-        molde:4,
-        peso:460,
-        conTapa:false,
-        tiempoHorneo:23,
-        tiempoEnf:60,
-        golpes:77,
-        rebanadas:10,
-        marca:"bimbo"
-    },
-    Artesano={
-        nombre:"Artesano bimbo 400g",
-        molde:4,
-        peso:450,
-        conTapa:false,
-        tiempoHorneo:23,
-        tiempoEnf:60,
-        golpes:77,
-        rebanadas:10,
-        marca:"bimbo"
-    }
-]
