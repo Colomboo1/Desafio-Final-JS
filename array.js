6a0d852835faf48d72edc0183a1c9019d80e309e
Andres Latorre <91092724+Colomboo1@users.noreply.github.com>
2022-04-12 20:30:58 -0300

    correcion de inner de producto y creacion storage

--- array.js
+++ array.js
@@ -176,4 +176,4 @@ const productos=[
     }
 ]
 
-module.exports = { productosA };
+//module.exports = { productosA };
--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 
 // SE POPULA EL SELECT ANTES DE QUE CARGUE LA PAGINA
 window.onload = () => {
+    uploadList();
     populateProducts();
 
 };
+//FUNCION DE CARGA DE ARRAY AL LOCALSTORAGE
+const uploadList= () =>{
+    localStorage.setItem('productos', JSON.stringify(productos));
+    console.log("carga en el storage");
+   
+}
 
+//SE GUARDA LISTA DEL STORAGE EN UNA CONSTANTE
+const productosStorage= JSON.parse(localStorage.getItem('productos'));
 
 // SE POPULA EL SELECT DINAMICAMENTE CON PRODUCTOS PROVENIENTES DEL ARRAY EN ARRAY.JS
 const populateProducts = () => {
     const products = document.getElementById("productList");
-    productos.map(producto => {
+    productosStorage.map(producto => {
         const option = document.createElement("option");
         option.value = producto.id;
         option.innerText = producto.nombre;
@@ -21,18 +30,20 @@ const populateProducts = () => {
 function getElementOfList() {
     
     const valueList = document.getElementById("productList").value;
-    const producto = productos.filter((producto) => producto.id==valueList)[0]; 
+    const producto = productosStorage.filter((producto) => producto.id==valueList)[0]; 
     return producto;
     
 }
 
+// CALCULA LA CANTIDAD DE MOLDES POR COLUMPIO
 function loadoven() {
     let producto = getElementOfList()
     console.log("llamada funcion ladoven ", producto.molde);
     let loader= parseInt(((producto.golpes/producto.molde)*producto.tiempoHorneo)/32);
-    return alert(loader+" Son los moldes que debe cargar");
+    return alert(loader+" moldes que debe cargar por columpio");
 } 
 
+// CALCULA CANTIDAD DE MOLDES QUE RECORRE LA LINEA
 function moldcount(){
     let producto = getElementOfList()
     let countmold= parseInt(producto.golpes/producto.molde);
@@ -40,7 +51,7 @@ function moldcount(){
    
 }
 
-
+// CALCULA LOS MIN. CON RESPECTOA  LOS KILOS DE BAJA
 function masstime(){
     let producto = getElementOfList()
     mass=document.getElementById("baja").value;
@@ -48,11 +59,10 @@ function masstime(){
     return alert("son: "+ totaldrop + " minutos de IPFM");
 }
 
+// IMPRIME LAS CARACTERISTICAS DEL PRODUCTO SELECCIONADO
 const resumeProduct= () => {
 let producto = getElementOfList()
-for (const clave in producto) {
-    console.log(`${clave}: ${producto[clave]}`);
-  }
+
 const products = document.getElementById("productSelected");
 for (let clave in producto){
     const p = document.createElement("p");
